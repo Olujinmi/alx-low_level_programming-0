42c75153890df0d395a3b19a8ced6f7e492e1ff6
jinmi <jinmiodunmbaku@gmail.com>
2022-09-09 15:38:42 -0700

    Super

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print a to z lowercase
+*
+* Description - Print a-z lower case
+* Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+	return (0);
+}
