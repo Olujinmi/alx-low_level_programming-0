02d5f7ca603f950f75e7ec4be915d6fdd7249f11
jinmi <jinmiodunmbaku@gmail.com>
2022-09-09 11:22:06 -0700

    Tree

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
-* main - Prints the phrase "Programming is like building a multilingual puzzle
+* main - Prints Programming is like building a multilingual puzzle
 * Return: returns 0
 */
 int main(void)
