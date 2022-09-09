2b45fbf55f37a1679d87a33057b312fba581f43a
jinmi <jinmiodunmbaku@gmail.com>
2022-09-09 13:22:39 -0700

    RED

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**                                                                                                                  
+* main - entry point                                                                                                 
+* Return: Always 0 (Success)                                                                                         
+*/
+int main()
+{
+	for(int A = 'a'; A <= 'z'; A++)
+	{
+		putchar(A);
+	}
+}
