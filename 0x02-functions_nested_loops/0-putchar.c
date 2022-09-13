640737842bb89ff9c779ef45827b370cafdef94d
jinmi <jinmiodunmbaku@gmail.com>
2022-09-13 14:16:46 -0700

    y

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  *  * main - print putchar
  *   * Return: always 0
- *    */
+*/
 
 int main(void)
 {
@@ -15,8 +15,8 @@ int main(void)
 		_putchar('h');
 		_putchar('a');
 		_putchar('r');
-		_putchar('\n');		
+		_putchar('\n');
 
-	       	return (0);
+		return (0);
 
 }
