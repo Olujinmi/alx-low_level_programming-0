1d1ec8dc28c9001402fed99ff82733570d62b163
jinmi <jinmiodunmbaku@gmail.com>
2022-09-13 15:24:36 -0700

    pushit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _islower - in lowercase
+* c: the value
+* Return: 0
+*/
+
+int _islower(int c);
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
