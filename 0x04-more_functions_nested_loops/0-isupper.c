da7f9060cb680820e0e1dce34b04047397402c22
jinmi <jinmiodunmbaku@gmail.com>
2022-09-15 16:12:08 -0700

    its late

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* int _isupper(int c) - checks for upper case
+* Return: 0
+*/
+
+int_isupper(int c)
+{
+int c;
+
+if ((c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else 
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+more oops and nests
