5c2d1f77dc5e84244fa9be248d5725ce14a148b7
jinmi <jinmiodunmbaku@gmail.com>
2022-09-15 16:19:35 -0700

    my eyes...they tire

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isdigit - checks for upper case
+* @c: number to be checked
+* Return: 1 or 0 depends
+*/
+
+int _isdigit(int c)
+{
+	if ((c >= '0' && c <= '9'))
+	{
+	return (1);
+	}
+
+	return (0);
+}
