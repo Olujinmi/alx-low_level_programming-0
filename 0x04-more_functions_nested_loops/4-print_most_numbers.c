1ea1b4b7c30d78dc93a1567208b2fc5c468051e3
jinmi <jinmiodunmbaku@gmail.com>
2022-09-15 17:01:16 -0700

    fd

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints 0 -9
+*
+* Return: to the void
+*/
+
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 0; i++)
+{
+	if ((i == 2 || i == 4))
+	{
+		continue;
+	}
+}
+	_putchar(i + '0');
+}
+
+_putchar('\n');
+}
