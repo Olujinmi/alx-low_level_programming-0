d56f3e328c6b4447de26093c31af6a58e894472b
jinmi <jinmiodunmbaku@gmail.com>
2022-09-15 17:14:07 -0700

    likey

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,15 @@
 
 void print_most_numbers(void)
 {
-int i;
+	int i;
 
-for (i = 0; i <= 9; i++)
-{
-	if ((i == 2) || (i == 4))
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i + '0');
+		if ((i == 2) || (i == 4)i)
+		{
+			_putchar(i + '0');
+		}
 	}
-}
 
-_putchar('\n');
+	_putchar('\n');
 }
