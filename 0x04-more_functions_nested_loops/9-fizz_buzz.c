93f63f07ad61e339edbe1e36bfc4a23b7b644e9e
jinmi <jinmiodunmbaku@gmail.com>
2022-09-15 17:52:43 -0700

    gf

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+*main - programmer acid test
+*Return: returns 0
+*/
+
+int main(void)
+{
+	int num = 1;
+
+	while (num++ < 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
