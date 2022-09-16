60cacf5ea830f08f9b8330255e2023d7a3d3dabe
jinmi <jinmiodunmbaku@gmail.com>
2022-09-15 18:17:18 -0700

    try

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,34 @@
-#include <stdio.h>
-
-/**
-*main - programmer acid test
-*Return: returns 0
-*/
-
-int main(void)
-{
-	int num = 1;
-
-	while (num++ <= 100)
-	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", num);
-		}
-	}
-	printf("\n");
-
-	return (0);
+#include <stdio.h>                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                             
+/**                                                                                                                                                                                                                                          
+*main - programmer acid test                                                                                                                                                                                                                 
+*Return: returns 0                                                                                                                                                                                                                           
+*/                                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                             
+int main(void)                                                                                                                                                                                                                               
+{                                                                                                                                                                                                                                            
+        int n = 1;                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                             
+        while (n++ <100)                                                                                                                                                                                                                 
+        {                                                                                                                                                                                                                                    
+                if ((n % 3) == 0)                                                                                                                                                                                                     
+                {                                                                                                                                                                                                                            
+                        printf("Fizz ");                                                                                                                                                                                                
+                }                                                                                                                                                                                                                            
+                else if ((n % 5) == 0)                                                                                                                                                                                                     
+                {                                                                                                                                                                                                                            
+                        printf("Buzz ");                                                                                                                                                                                                     
+                }                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+                else if ((n % 3 == 0) && (n % 5 == 0))                                                                                                                                                                                        
+                {                                                                                                                                                                                                                            
+                        printf("FizzBuzz ");     
+                }
+                else                                                                                                                                                                                                                         
+                {                                                                                                                                                                                                                            
+                        printf("%d ", n);                                                                                                                                                                                                  
+                }                                                                                                                                                                                                                            
+        }                                                                                                                                                                                                                                    
+        printf("\n");                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                             
+        return (0);                                                                                                                                                                                                                          
 }
