8bc25d00d09c42288c84d7a1a74b6eeea6a97d7d
jinmi <jinmiodunmbaku@gmail.com>
2022-09-15 18:42:33 -0700

    trees

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,32 @@
-#include <stdio.h>                                                                                                                                                                                                                           
-                                                                                                                                                                                                                                             
-/**                                                                                                                                                                                                                                          
-*main - programmer acid test                                                                                                                                                                                                                 
-*Return: returns 0                                                                                                                                                                                                                           
-*/                                                                                                                                                                                                                                           
-                                                                                                                                                                                                                                             
-int main(void)                                                                                                                                                                                                                               
-{                                                                                                                                                                                                                                            
-        int n = 1;                                                                                                                                                                                                                         
-                                                                                                                                                                                                                                             
-        while (n++ <100)                                                                                                                                                                                                                 
-        {                                                                                                                                                                                                                                    
-                if ((n % 3) == 0)                                                                                                                                                                                                     
-                {                                                                                                                                                                                                                            
-                        printf("Fizz ");                                                                                                                                                                                                
-                }                                                                                                                                                                                                                            
-                else if ((n % 5) == 0)                                                                                                                                                                                                     
-                {                                                                                                                                                                                                                            
-                        printf("Buzz ");                                                                                                                                                                                                     
-                }                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-                else if ((n % 3 == 0) && (n % 5 == 0))                                                                                                                                                                                        
-                {                                                                                                                                                                                                                            
-                        printf("FizzBuzz ");     
-                }
-                else                                                                                                                                                                                                                         
-                {                                                                                                                                                                                                                            
-                        printf("%d ", n);                                                                                                                                                                                                  
-                }                                                                                                                                                                                                                            
-        }                                                                                                                                                                                                                                    
-        printf("\n");                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                             
-        return (0);                                                                                                                                                                                                                          
-}
+#include <stdio.h>
+/**
+* main - programmer acid test
+* Return: 0
+*/
+
+int main(void)
+{
+	int n = 1;
+
+	while (n++ <= 100)
+	{
+		if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+
+	return (0);
